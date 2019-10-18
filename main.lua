c51acda68281bb39602f0f810e6b214273859a5f
phlogios <phlogios@gmail.com>
2019-10-18 22:07:33 +0200

    add snake sprites

--- main.lua
+++ main.lua
@@ -1,16 +1,45 @@
-x = 0
-y = 0
+snakeheadImg = love.graphics.newImage("snakehead.png")
+snakebodyImg = love.graphics.newImage("snakebody-sheet.png")
+snaketailImg = love.graphics.newImage("snaketail.png")
+snakecornerImg = love.graphics.newImage("snakecorner.png")
+headQuads = {
+    love.graphics.newQuad(0, 0, 16, 16, snakeheadImg:getDimensions()),
+    love.graphics.newQuad(16, 0, 16, 16, snakeheadImg:getDimensions())
+}
+bodyQuads = {
+    love.graphics.newQuad(0, 0, 16, 16, snakebodyImg:getDimensions()),
+    love.graphics.newQuad(16, 0, 16, 16, snakebodyImg:getDimensions()),
+    love.graphics.newQuad(32, 0, 16, 16, snakebodyImg:getDimensions()),
+    love.graphics.newQuad(48, 0, 16, 16, snakebodyImg:getDimensions()),
+}
+tailQuads = {
+    love.graphics.newQuad(0, 0, 16, 16, snaketailImg:getDimensions()),
+    love.graphics.newQuad(16, 0, 16, 16, snaketailImg:getDimensions()),
+    love.graphics.newQuad(32, 0, 16, 16, snaketailImg:getDimensions()),
+    love.graphics.newQuad(48, 0, 16, 16, snaketailImg:getDimensions())
+}
+cornerQuads = {
+    love.graphics.newQuad(0, 0, 16, 16, snakecornerImg:getDimensions()),
+    love.graphics.newQuad(16, 0, 16, 16, snakecornerImg:getDimensions()),
+    love.graphics.newQuad(32, 0, 16, 16, snakecornerImg:getDimensions()),
+    love.graphics.newQuad(48, 0, 16, 16, snakecornerImg:getDimensions()),
+}
+
+tileWidth = 16
+tileHeight = 16
+tilesX = 320 / tileWidth
+tilesY = 240 / tileHeight
 
 direction = 0
-directionLastTick = 0
 lengthOfTick = 0.5
 tickMultiplicationPerApple = 0.985
 tickTime = 0
-tileSize = 10
+tickAccumulator = 0
 gameStarted = false
 gamePaused = false
 playerDead = false
 shouldQuit = false
+ate = false
 
 score = 0
 
@@ -62,26 +91,32 @@ end
 function setupGame()
     playerDead = false
     score = 0
-    x = 16
-    y = 12
+    x = math.floor(tilesX / 2)
+    y = math.floor(tilesY / 2)
     direction = 0
-    lengthOfTick = 0.5
+    lengthOfTick = 0.4
     tickTime = 0
     snakeBits = {
         {
-            x = 16,
-            y = 13
+            x = x,
+            y = y+1,
+            directionLastTick = 0
+        },
+        {
+            x = x,
+            y = y+2,
+            directionLastTick = 0
         }
     }
 
     apples = {
         {
-            x = math.floor(love.math.random()*32),
-            y = math.floor(love.math.random()*24)
+            x = math.floor(love.math.random()*tilesX),
+            y = math.floor(love.math.random()*tilesY)
         },
         {
-            x = math.floor(love.math.random()*32),
-            y = math.floor(love.math.random()*24)
+            x = math.floor(love.math.random()*tilesX),
+            y = math.floor(love.math.random()*tilesY)
         }
     }
 end
@@ -94,43 +129,64 @@ function love.update(dt)
     if gameStarted == true and gamePaused == false then
         tickTime = tickTime + dt
         if tickTime > lengthOfTick then
-            directionLastTick = direction
+            tickAccumulator = tickAccumulator + 1
+            if ate then
+                table.insert(snakeBits, {
+                    x = snakeBits[#snakeBits].x,
+                    y = snakeBits[#snakeBits].y,
+                    directionLastTick = snakeBits[#snakeBits].directionLastTick,
+                    corner = snakeBits[#snakeBits].corner
+                })
+            end
+            
             for i = #snakeBits, 2, -1 do
                 snakeBits[i].x = snakeBits[i-1].x
                 snakeBits[i].y = snakeBits[i-1].y
+                local newDirection = snakeBits[i-1].directionLastTick
+                snakeBits[i].directionLastTick = newDirection
+                snakeBits[i].corner = snakeBits[i-1].corner
             end
-            snakeBits[1].x = x
-            snakeBits[1].y = y
+            
+            if snakeBits[1].directionLastTick ~= direction then
+                local od = snakeBits[1].directionLastTick
+                local nd = direction
+                if (od == 0 and nd == 1) or (od == 3 and nd == 2) then snakeBits[2].corner = 1 end
+                if (od == 0 and nd == 3) or (od == 1 and nd == 2) then snakeBits[2].corner = 2 end
+                if (od == 2 and nd == 3) or (od == 1 and nd == 0) then snakeBits[2].corner = 3 end
+                if (od == 2 and nd == 1) or (od == 3 and nd == 0) then snakeBits[2].corner = 4 end
+            else
+                snakeBits[2].corner = 0
+            end
+            snakeBits[1].directionLastTick = direction
+
             if direction == 0 then
-                y = y - 1
-            elseif direction == 1 then
-                y = y + 1
+                snakeBits[1].y = snakeBits[1].y - 1
             elseif direction == 2 then
-                x = x - 1
+                snakeBits[1].y = snakeBits[1].y + 1
             elseif direction == 3 then
-                x = x + 1
+                snakeBits[1].x = snakeBits[1].x - 1
+            elseif direction == 1 then
+                snakeBits[1].x = snakeBits[1].x + 1
             end
-            if x > 31 then x = 0 end
-            if x < 0 then x = 31 end
-            if y > 23 then y = 0 end
-            if y < 0 then y = 23 end
+            if snakeBits[1].x > tilesX-1 then snakeBits[1].x = 0 end
+            if snakeBits[1].x < 0 then snakeBits[1].x = tilesX-1 end
+            if snakeBits[1].y > tilesY-1 then snakeBits[1].y = 0 end
+            if snakeBits[1].y < 0 then snakeBits[1].y = tilesY-1 end
             
             for i = 2, #snakeBits do
-                if snakeBits[i].x == x and snakeBits[i].y == y then
+                if snakeBits[i].x == snakeBits[1].x and snakeBits[i].y == snakeBits[1].y then
                     gameStarted = false
                     playerDead = true
                     return
                 end
             end
             
+            ate = false
             for i = 1, #apples do
-                if apples[i].x == x and apples[i].y == y then
-                    table.insert(snakeBits, {
-                        x = snakeBits[1].x,
-                        y = snakeBits[1].y
-                    })
-                    apples[i].x = math.floor(love.math.random()*32)
-                    apples[i].y = math.floor(love.math.random()*24)
+                if apples[i].x == snakeBits[1].x and apples[i].y == snakeBits[1].y then
+                    ate = true
+                    apples[i].x = math.floor(love.math.random()*tilesX)
+                    apples[i].y = math.floor(love.math.random()*tilesY)
                     lengthOfTick = lengthOfTick * tickMultiplicationPerApple
                     score = score + 100
                 end
@@ -143,14 +199,19 @@ end
 function love.draw()
     if gameStarted then
         love.graphics.setColor(255, 255, 255)
-        love.graphics.rectangle("fill", x * tileSize, y * tileSize, tileSize, tileSize)
-        for i, bit in ipairs(snakeBits) do
-            love.graphics.rectangle("fill", bit.x*tileSize, bit.y*tileSize, tileSize, tileSize)
+        for i = 2, #snakeBits-1 do
+            if snakeBits[i].corner == 0 then
+                love.graphics.draw(snakebodyImg, bodyQuads[(tickAccumulator + i) % #bodyQuads + 1], (snakeBits[i].x * tileWidth)+tileWidth/2, (snakeBits[i].y * tileHeight)+tileHeight/2, (math.pi/2)*snakeBits[i].directionLastTick, 1, 1, 8, 8)
+            else
+                love.graphics.draw(snakecornerImg, cornerQuads[snakeBits[i].corner], (snakeBits[i].x * tileWidth)+tileWidth/2, (snakeBits[i].y * tileHeight)+tileHeight/2, 0, 1, 1, 8, 8)
+            end
         end
+        love.graphics.draw(snaketailImg, tailQuads[tickAccumulator % #tailQuads + 1], (snakeBits[#snakeBits].x * tileWidth)+tileWidth/2, (snakeBits[#snakeBits].y * tileHeight)+tileHeight/2, (math.pi/2)*snakeBits[#snakeBits-1].directionLastTick, 1, 1, 8, 8)
+        love.graphics.draw(snakeheadImg, headQuads[tickAccumulator % #headQuads + 1], (snakeBits[1].x * tileWidth)+tileWidth/2, (snakeBits[1].y * tileHeight)+tileHeight/2, (math.pi/2)*snakeBits[1].directionLastTick, 1, 1, 8, 8)
         
         love.graphics.setColor(255, 0, 0)
         for i = 1, #apples do
-            love.graphics.rectangle("fill", apples[i].x*tileSize, apples[i].y*tileSize, tileSize, tileSize)
+            love.graphics.rectangle("fill", apples[i].x*tileWidth, apples[i].y*tileHeight, tileWidth, tileHeight)
         end
     end
     love.graphics.setFont(font)
@@ -178,16 +239,16 @@ end
 function love.keypressed( key )
     if gameStarted and not gamePaused then
         if key == "up" then
-            if directionLastTick ~= 1 then direction = 0 end
+            if snakeBits[1].directionLastTick ~= 2 then direction = 0 end
         end
         if key == "down" then
-            if directionLastTick ~= 0 then direction = 1 end
+            if snakeBits[1].directionLastTick ~= 0 then direction = 2 end
         end
         if key == "left" then
-            if directionLastTick ~= 3 then direction = 2 end
+            if snakeBits[1].directionLastTick ~= 1 then direction = 3 end
         end
         if key == "right" then
-            if directionLastTick ~= 2 then direction = 3 end
+            if snakeBits[1].directionLastTick ~= 3 then direction = 1 end
         end
     else
         if key == "up" then
--- make_love.sh
+++ make_love.sh
@@ -4,25 +4,9 @@ OPK_NAME=${NAME}.opk
 
 echo ${OPK_NAME}
 
-# create default.gcw0.desktop
-cat > default.gcw0.desktop <<EOF
-[Desktop Entry]
-Name=LoveSnake
-Comment=Snake in Love
-Exec=love ./
-Terminal=false
-Type=Application
-StartupNotify=true
-Icon=lovesnake
-Categories=games;
-EOF
-
 FLIST="lovesnake.png Resource-Imagefont.png conf.lua main.lua default.gcw0.desktop"
 
 # create opk
 
 rm -f ${OPK_NAME}
 mksquashfs ${FLIST} ${OPK_NAME} -all-root -no-xattrs -noappend -no-exports
-
-cat default.gcw0.desktop
-rm -f default.gcw0.desktop
